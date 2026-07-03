6fa671d49ba2f6f1b21e19fbbfb0c827b2ef642e
om <ooommm1410@gmail.com>
2026-07-03 21:52:10 +0530

    Render Backend Link

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,5 @@
 export const CONFIG = {
-    API_BASE_URL: "http://localhost:8000/api",
+    // API_BASE_URL: "http://localhost:8000/api",
+    API_BASE_URL: "https://lumosai-backend-tn6r.onrender.com",
     POLL_INTERVAL: 3000 // In case we need it, though currently synchronous
 };
